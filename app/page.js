26f79145372f73ac42bf1655078ec57480676913
Collin <collin-chua@hotmail.com>
2023-03-08 13:31:44 +0800

    Added styling and more flexibility to button icon

--- app/page.js
+++ app/page.js
@@ -1,17 +1,14 @@
 import Button from "@/components/Button"
-import {GoPlusSmall} from "react-icons/go"
 import Banner from "@/components/Banner"
 
 export default function Home() {
-  const plusIcon = <span className="text-center m-auto"><GoPlusSmall size={23}/></span>
-
   return (
     <main className="py-5">
       <Banner>Personal Financial App</Banner>
 
       <div className="container mx-auto flex justify-between py-5 border-b">
         <div className="left flex gap-3">
-          <Button className='flex'>Add Statement {plusIcon}</Button>
+          <Button icon className='flex'>Add Statement</Button>
         </div>
       </div>
     </main>
--- components/Button.js
+++ components/Button.js
@@ -1,10 +1,13 @@
 'use client'
+import {GoPlusSmall} from "react-icons/go"
 import classNames from "classnames"
 
-const Button = ({ children, ...rest }) => {
+const Button = ({ children, icon, ...rest }) => {
     const classes = classNames('bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-100 hover:border-indigo-500 hover:text-gray-800 duration-300', rest.className)
+    const plusIcon = <span className="text-center m-auto"><GoPlusSmall size={23}/></span>
+
   return (
-    <button {...rest} className={classes}>{children}</button>
+    <button {...rest} className={classes}>{children} {icon && plusIcon}</button>
   )
 }
 
