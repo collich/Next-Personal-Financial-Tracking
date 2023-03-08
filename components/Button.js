78ceadb7f1a3562e3f77223605420354870cd157
Collin <collin-chua@hotmail.com>
2023-03-08 13:13:33 +0800

    Added styling to buttons component

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,10 @@
-const Button = ({ children }) => {
+'use client'
+import classNames from "classnames"
+
+const Button = ({ children, ...rest }) => {
+    const classes = classNames('bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-100 hover:border-indigo-500 hover:text-gray-800 duration-300', rest.className)
   return (
-    <button>{children}</button>
+    <button {...rest} className={classes}>{children}</button>
   )
 }
 
