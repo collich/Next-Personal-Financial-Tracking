4e7e6cdb5a70a9dfea5a5bb4cc540caab07c167f
Collin <collin-chua@hotmail.com>
2023-03-09 13:31:51 +0800

    Added a little bit more styling to the banner

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Table from "../components/table/Table"
 export default function Home() {
   return (
     <main className="py-5">
-      <Banner>Personal Financial App</Banner>
+      <Banner className="bg-gradient-to-r from-orange-200 to-indigo-400 container mx-auto rounded-md">Personal Financial App</Banner>
 
       <div className="container mx-auto flex justify-between py-5 border-b">
         <div className="left flex gap-3">
--- components/Banner.js
+++ components/Banner.js
@@ -4,7 +4,9 @@ import classNames from "classnames"
 const Banner = ({children, className}) => {
     const classes = classNames('text-xl md:text-5xl text-center font-bold py-10', className)
   return (
-    <h1 className={classes}>{children}</h1>
+    <div>
+      <h1 className={classes}>{children}</h1>
+    </div>
   )
 }
 
