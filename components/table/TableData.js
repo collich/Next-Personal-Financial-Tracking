817dae1fca18be4686f1b8cd62da2ec148d66f7d
Collin <collin-chua@hotmail.com>
2023-03-09 13:03:11 +0800

    Added a client side Table Data calculate and added another dummy data

--- components/table/TableData.js
+++ components/table/TableData.js
@@ -10,36 +10,20 @@ const TableData = () => {
       expenses: 256,
       investment: 500,
       savings: 0,  
-    }
+    },
+    {
+      date: `${(new Date(Date.now())).toLocaleString()}`,
+      // date: `${Date.getDate() > 10 ? Date.getDate(): `0${Date.getDate()}-${Date.getMonth() > 10 ? Date.getMonth(): `0${Date.getMonth() + 1}`}-${Date.getFullYear()}`}`,
+      salary: 5000,
+      CPF: 20,
+      expenses: 300,
+      investment: 700,
+      savings: 0,  
+    },
   ]
 
   return (
-    <tr className="bg-gray-50 text-center">
-        <td className="px-16 py-2 items-center">
-          <span className="text-center ml-2 font-semibold">{moneyList[0].date}</span>
-        </td>
-
-        <td className="px-16 py-2 items-center">
-          <span className="text-center ml-2 font-semibold">${moneyList[0].salary}</span>
-        </td>
-
-        <td className="px-16 py-2 items-center">
-          <span className="text-center ml-2 font-semibold">${moneyList[0].salary * 20/100}</span>
-        </td>
-
-        <td className="px-16 py-2 items-center">
-          <span className="text-center ml-2 font-semibold">${moneyList[0].expenses}</span>
-        </td>
-
-        <td className="px-16 py-2 items-center">
-          <span className="text-center ml-2 font-semibold">${moneyList[0].investment}</span>
-        </td>
-
-        <td className="px-16 py-2 items-center">
-          <span className="text-center ml-2 font-semibold">${moneyList[0].salary - (moneyList[0].salary * 20/100) - moneyList[0].expenses - moneyList[0].investment}</span>
-        </td>
-        {/* <TableDataOut data={moneyList}/> */}
-    </tr>
+    <TableDataOut data={moneyList}/>
   )
 }
 
--- components/table/TableDataOut.js
+++ components/table/TableDataOut.js
@@ -1,15 +1,37 @@
+'use client'
+
 const TableDataOut = ({ data }) => {
-  // const renderData = data.map( datum => {
+  const renderData = data.map( (datum, index) => {
+    return (
+      <tr className="bg-gray-50 text-center" key={index}>
+        <td className="px-16 py-2 items-center">
+          <span className="text-center ml-2 font-semibold">{datum.date}</span>
+        </td>
 
-  // })
-  console.log(data);
+        <td className="px-16 py-2 items-center">
+          <span className="text-center ml-2 font-semibold">${datum.salary}</span>
+        </td>
 
-  return (
-    // {renderData}
-    <div>
+        <td className="px-16 py-2 items-center">
+          <span className="text-center ml-2 font-semibold">${datum.salary * 20/100}</span>
+        </td>
 
-    </div>
-  )
+        <td className="px-16 py-2 items-center">
+          <span className="text-center ml-2 font-semibold">${datum.expenses}</span>
+        </td>
+
+        <td className="px-16 py-2 items-center">
+          <span className="text-center ml-2 font-semibold">${datum.investment}</span>
+        </td>
+
+        <td className="px-16 py-2 items-center">
+          <span className="text-center ml-2 font-semibold">${datum.salary - (datum.salary * 20/100) - datum.expenses - datum.investment}</span>
+        </td>
+      </tr>
+    )
+  })
+
+  return renderData
 }
 
 export default TableDataOut
