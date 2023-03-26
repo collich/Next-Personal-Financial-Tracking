6212d7fdb72e8a9a893551ddc3f4a9c713c3ffd3
Collin <collin-chua@hotmail.com>
2023-03-26 17:39:58 +0800

    Just a change in title, need to do patching to database next

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Table from "../components/table/Table"
 export default function Home() {
   return (
     <main className="py-5">
-      <Banner className="bg-gradient-to-r from-orange-200 to-indigo-400 container mx-auto rounded-md">Personal Financial App</Banner>
+      <Banner className="bg-gradient-to-r from-orange-200 to-indigo-400 container mx-auto rounded-md">Personal Financial Tracking App</Banner>
 
       <FormPage/>
 
