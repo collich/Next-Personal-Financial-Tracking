5181764e14df3b0b8fa11877383dab6c3920af23
Collin <collin-chua@hotmail.com>
2023-03-28 16:33:52 +0800

    Added delete method on for prisma

--- pages/api/finances/[id]/index.js
+++ pages/api/finances/[id]/index.js
@@ -1,8 +1,8 @@
-import { getStatementById, updateStatement } from "@/lib/prisma/statements";
+import { getStatementById, updateStatement, deleteStatement } from "@/lib/prisma/statements";
 
 export default async function handler(req, res) {
-    console.log(req.body);
-    console.log(req.query.id);
+    // console.log(req.body);
+    // console.log(req.query.id);
     if (req.method === "GET") {
         try {
             const id = req.query.id
@@ -25,6 +25,21 @@ export default async function handler(req, res) {
             return res.status(200).json({
                 status: "Success",
                 statement: updatedStatement,
+            })
+        } catch (error) {
+            return res.status(500).json({
+                message: 'Internal Server Error'
+            })
+        }
+    }
+
+    if (req.method === "DELETE"){
+        try {
+            const id = req.query.id
+            const deletedStatement = await deleteStatement(id)
+            return res.status(200).json({
+                status: "Success",
+                statement: deletedStatement,
             })
         } catch (error) {
             return res.status(500).json({
